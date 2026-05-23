8fcb9bc1e797593847dbfcd1b87665632b720ebe
mhassan-swe <mhassan.swe@gmail.com>
2026-05-23 13:19:55 +0500

    refactor signUp function to improve validation and error messages

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,15 +3,33 @@ import User from '../models/user.model.js'
 export const signUp = (req,res) => {
     const { fullname, email, password } = req.body;
     try{
-        if( password.length < 6 ){
-            return res.status(400).json({"Password must be atleat 6 digits"})
-        }
         if(!fullname || !email || !password){
-             return res.status(400).json({"All fields are required"})
+             return res.status(400).json({message:"All fields are required"})
         }
 
+        if( password.length < 6 ){
+            return res.status(400).json({message:"Password must be atleat 6 digits"})
+        }
+        
 
+        const user = await User.findOne({email});
 
+        if(user){
+            return res.status(400).json({message:"User already exist"})
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword =await bcrypt.hash(password,salt)
+
+        const newUser = new User({
+            fullname,
+            email,
+            password:hashedPassword
+        })
+
+       await newUser.save();
+
+        
     }
     catch(error){
         return res.status(500).json({message:error.message})
