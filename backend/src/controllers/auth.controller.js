4d7d5af9839246c66121900c05ecbcdef29e851b
mhassan-swe <mhassan.swe@gmail.com>
2026-05-24 13:05:40 +0500

    implement logout functionality with cookie clearing and response message

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -81,3 +81,18 @@ export const login =async (req,res) => {
         return res.status(500).json({message:error.message});
     }
 }
+
+export const logout = (req,res) => {
+    try{
+        res.clearCookie('jwt', {
+            httpOnly:true,
+            sameSite:'strict',
+            secure: process.env.NODE_ENV !== "development"
+
+        } );
+       return res.status(200).json({message:'Session ended'});
+    }
+    catch(error){
+       return res.status(500).json({message:error.message});
+    }
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { signUp, login } from '../controllers/auth.controller.js'
+import { signUp, login ,logout } from '../controllers/auth.controller.js'
 
 const router = Router();
 
 router.post('/sign-up',signUp);
 router.post('/login',login);
+router.post('/logout',logout);
 
 
 
