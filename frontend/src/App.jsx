09a5e1f496ff961232b4fe1ed551480df042ebe5
mhassan-swe <mhassan.swe@gmail.com>
2026-06-07 14:57:39 +0500

    feat: enhance routing with authentication checks and add page titles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import NavBar from './components/NavBar.jsx'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useAuthStore } from './store/useAuthStore.js'
 import { Loader } from 'lucide-react'
@@ -14,7 +13,7 @@ import ProfilePage from './pages/ProfilePage.jsx'
 
 
 const App = () => {
-  const { authUser, checkAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
   useEffect( () => {
     checkAuth()
@@ -22,18 +21,26 @@ const App = () => {
 
   console.log(authUser)
 
-  if( isCheckingAuth && !authUser) return
+  if( isCheckingAuth && !authUser) return(
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className='size-10 animate-spin'/>
+    </div>
+  )
   return (
     <div>
       <NavBar/>
 
       <Routes>
 
-        <Route path='/' element={< HomePage />} />
-        <Route path='/signup' element={< SignUpPage />} />
-        <Route path='/login' element={< LoginPage />} />
+        <Route path='/' element={ authUser ? < HomePage /> : <Navigate to="/login"/> } />
+
+        <Route path='/signup' element={authUser ? < SignUpPage /> : <Navigate to="/"/> } />
+
+        <Route path='/login' element={ authUser ? < LoginPage /> : <Navigate to="/login"/>} />
+        
         <Route path='/settings' element={< SettingsPage />} />
-        <Route path='/profile' element={< ProfilePage />} />
+        
+        <Route path='/profile' element={authUser ? < ProfilePage /> : <Navigate to="/login"/>} />
 
       </Routes>
     </div>
