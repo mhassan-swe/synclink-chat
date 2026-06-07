51a8eb345197dbb93eccab5721f1341c0b1d9ca4
mhassan-swe <mhassan.swe@gmail.com>
2026-06-07 15:09:58 +0500

    feat: implement sign-up form structure and validation placeholders

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
 const SignUpPage = () => {
+  const [showPassword, setShowPassword] = useSate(false);
+  const [formData, setFormData] = useState({
+    fullName="",
+    email:"",
+    password:"",
+  })
+
+  const {signup , isSigningUp } = useAuthStore();
+
+  const validateForm = () => {}
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  
   return (
-    <div>
-      
+    <div className='min-h-screen grid lg:grid-cols-2'>
+      <div className='flex flex-col justify-center items-center p-6 sm:p-12 '>
+        <div className='w-full max-w-md space-y-8'>
+          
+        </div>
+      </div>
     </div>
   )
 }
