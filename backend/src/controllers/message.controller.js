879a757f26824ac998710d103b7d8a261ecdbb8e
mhassan-swe <mhassan.swe@gmail.com>
2026-06-07 13:17:18 +0500

    feat: implement image upload in sendMessage function and fix response handling

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from 'cloudinary'
 
 
 export const getUsersForSideBar = async (req,res) => {
@@ -38,9 +39,29 @@ export const getMessages = async (req,res) => {
 
 export const sendMessage = async (req,res) => {
     try{
-        
+        const { text, image } = req.body;
+        const { id:receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl
+        })
+
+        await newMessage.save();
+
+        return res.status(200).json(newMessage)
     } 
     catch(error) {
-        
+        return res.status(500).json({messsage:error.message});
     }
 }
