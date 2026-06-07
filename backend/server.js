4be86831b1c3cbb39834e595e46c24ae8b76f734
mhassan-swe <mhassan.swe@gmail.com>
2026-06-07 14:36:47 +0500

    feat: add CORS support, implement authentication store, and update dependencies

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,19 @@ import dotenv from 'dotenv'
 import authRoute from './src/routes/auth.route.js'
 import messageRoute from './src/routes/message.route.js'
 import cookieParser from 'cookie-parser'
-
+import cors from 'cors'
 const app = express(); 
 
 dotenv.config();
 
 app.use(express.json());// allow json data to be parsed into req.body
 app.use(cookieParser());
+app.use( cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}) )
+
+
 app.use('/api/auth',authRoute);
 app.use('/api/message',messageRoute);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import NavBar from './components/NavBar.jsx'
 import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useAuthStore } from './store/useAuthStore.js'
+import { Loader } from 'lucide-react'
+
 
 import HomePage from './pages/HomePage.jsx'
 import SignUpPage from './pages/SignUpPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
-import Settingspage from './pages/Settingspage.jsx'
+import SettingsPage from './pages/Settingspage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
 
+
 const App = () => {
+  const { authUser, checkAuth } = useAuthStore();
+
+  useEffect( () => {
+    checkAuth()
+  }, [checkAuth] );
+
+  console.log(authUser)
+
+  if( isCheckingAuth && !authUser) return
   return (
     <div>
       <NavBar/>
